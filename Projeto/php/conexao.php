95fc7b2be702ebdb85077c876fe2306e4a789826
lucasb96 <lucassbbrandao1996@gmail.com>
2022-12-02 12:33:24 -0300

    Implementação do PHP
    Página de Cadastro

--- Projeto/php/conexao.php
+++ Projeto/php/conexao.php
@@ -4,6 +4,21 @@ $usuario = "root";
 $senha = "";
 $bdnome = "projeto";
 
-$conn = mysqli_connect($servidor, $usuario, $senha);
+$conn = mysqli_connect($servidor, $usuario, $senha, $bdnome);
 $banco = mysqli_select_db($conn, $bdnome);
+
+/*$dbHost = 'localhost';
+$dbUsername = 'root';
+$dbPassword = '';
+$dbName = 'projeto';
+
+$conexao = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);*/
+
+/*if($conn->connect_errno){
+    echo "Erro!";
+}
+else {
+    echo "Conexão efetuada com sucesso!";
+}*/
+
 ?>
